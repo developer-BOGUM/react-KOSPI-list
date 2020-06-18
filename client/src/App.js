2cac0777268065cd54e0e62975c5c43338dc0347
김보검 <bogumk1206@gmail.com>
2020-06-18 13:27:46 +0900

    20.6.18 서버접속로딩구축

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import axios from 'axios';
 import './App.css';
 import Customer from './components/Customer';
 import Table from '@material-ui/core/Table';
@@ -7,14 +6,29 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  progress: {
+    margin: theme.spacing.unit * 2
+  }
+});
 
 
 class App extends React.Component {
 
   state = {
-    customers:""
+    customers:"",
+    completed:0
   }
   componentDidMount() {
+    this.timer = setInterval(this.progress, 20);
     this.callapi()
     .then(res => this.setState({customers: res}))
     .catch(err => console.log(err));
@@ -26,6 +40,12 @@ class App extends React.Component {
     const body = await response.json();
     return body;
   }
+
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({ completed: completed >= 100 ? 0 : completed + 1});
+  }
+
   render() {
     return(
         <Table>
@@ -52,7 +72,13 @@ class App extends React.Component {
                       jab={c.jab}
                     />
               );
-              }) : ""}
+              }) : 
+              <TableRow>
+                <TableCell colSpan="6" align="center">
+                  <CircularProgress  variant="determinate" value={this.state.completed} />
+                </TableCell>  
+              </TableRow>
+              }
           </TableBody>
         </Table>
     );
