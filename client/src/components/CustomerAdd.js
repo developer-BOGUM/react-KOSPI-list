a33c7ba3d5c3a14a6d6a90cc8fb12d7ba8a85962
김보검 <bogumk1206@gmail.com>
2020-06-19 11:55:54 +0900

    20.06.19 DB전송 테스트완료

--- client/src/components/CustomerAdd.js
+++ client/src/components/CustomerAdd.js
@@ -16,16 +16,25 @@ class CustomerAdd extends React.Component {
     }
 
     handleFormSubmit = (e) => {
-        e.preventDefailt()
+        e.preventDefault()
         this.addCustomer()
-        .then((response) => {
-            console.log(response.data);
+            .then((response) => {
+                console.log(response.data);
+            })
+        this.setState({
+            file: null,
+            userName:"",
+            birthday:"",
+            gender:"",
+            job:"",
+            fileName:""
         })
+        window.location.reload();
     }
 
     handleFileChange = (e) => {
         this.setState({
-            file: e.target.file[0],
+            file: e.target.files[0],
             fileName: e.target.value
         })
     }
@@ -40,7 +49,7 @@ class CustomerAdd extends React.Component {
         const url = '/api/customers';
         const formData = new FormData();
         formData.append('image', this.state.file);
-        formData.append('image', this.state.userName);
+        formData.append('name', this.state.userName);
         formData.append('birthday', this.state.birthday);
         formData.append('gender', this.state.gender);
         formData.append('job', this.state.job);
@@ -54,15 +63,15 @@ class CustomerAdd extends React.Component {
 
     render() {
         return (
-            <from onSubmit={this.handleFormSubmit}>
+            <form onSubmit={this.handleFormSubmit}>
                 <h1>고객 추가</h1>
                 프로필 이미지: <input type="file" name="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange}/><br/>
                 이름: <input type="text" name="userName" value={this.state.userName} onChange={this.handleValueChange}/><br/>
                 생년월일: <input type="text" name="birthday" value={this.state.birthday} onChange={this.handleValueChange}/><br/>
                 성별: <input type="text" name="gender" value={this.state.gender} onChange={this.handleValueChange}/><br/>
-                이름: <input type="text" name="job" value={this.state.job} onChange={this.handleValueChange}/><br/>
+                직업: <input type="text" name="job" value={this.state.job} onChange={this.handleValueChange}/><br/>
                 <button type="submit">추가하기</button>
-            </from>
+            </form>
         )
     }
 }
